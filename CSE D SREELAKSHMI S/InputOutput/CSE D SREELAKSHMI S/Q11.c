5c1bf7a660f561a66ba8b55d8e78ead66d3acb86
sreelakshmisatheesh197-coder <sreelakshmisatheesh197@gmail.com>
2025-08-30 10:16:11 +0530

    Add files via upload

---
+++ CSE D SREELAKSHMI S/InputOutput/CSE D SREELAKSHMI S/Q11.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	char a[50];
+	scanf("%c",&a);
+	printf("%d",a);
+	return 0;
+}
---
+++ CSE D SREELAKSHMI S/InputOutput/CSE D SREELAKSHMI S/Q14.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	int b=6;
+	printf("%d	%d",a,b);
+	return 0;
+}
